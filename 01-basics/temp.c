d28c380784a1ae6caf18b5c5153745fcedac8003
wmfelipe <wmfelipee@gmail.com>
2026-01-24 15:26:21 -0300

    Add first temperature scale in C

---
+++ 01-basics/temp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
+int main(void)
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;    /* lower limit of temperature scale */
+    upper = 300;  /* upper limit */
+    step = 20;    /* step size  */
+
+    fahr = lower;
+    while (fahr  <= upper) {
+        float celsius;
+        celsius = (5.0/9.0) * (fahr - 32);
+        printf("%d\t%.1f\n", fahr, celsius);
+        fahr = fahr + step;;
+
+    }
+    return 0;
+}
+    
